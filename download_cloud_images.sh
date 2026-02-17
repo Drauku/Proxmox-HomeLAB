306081b2a930a901a08a8bdbc11cdfe39fb63e21
Drauku <drauku@gmail.com>
2026-02-17 17:19:44 -0600

    moved image info to array, refactored download function to use array

--- download_cloud_images.sh
+++ download_cloud_images.sh
@@ -10,7 +10,7 @@ rst=$(tput sgr0)
 
 # Variables
 export ssh_keyfile="/etc/pve/priv/authorized_keys"
-export username="${1:-admin}"
+export username="${2:-admin}"
 storage=$(pvesm status | awk '/local-/{print $1; exit}')
 vm_confs="/etc/pve/qemu-server"
 vm_image="/var/lib/vz/template/iso"
@@ -24,10 +24,24 @@ dns0="192.168.186.253"
 dns1="9.9.9.9"
 dns2="1.1.1.1"
 
+# Distro definitions
+distros=(
+    "debian;9013;tmpl-debian-13;debian-13.qcow2;https://cloud.debian.org/images/cloud/bookworm/20260210-2384/debian-12-genericcloud-amd64-20260210-2384.qcow2;e5d776b9de352c89fbad4baec8bfd38a35c5905114a5f3b108946348cb44d869396d22e4a837a43afee4b11363d4759c358fb3e8a7cd07fa743ed6b663784fed"
+    "ubuntu;9025;tmpl-ubuntu-25;ubuntu-25-04.img;https://cloud-images.ubuntu.com/releases/plucky/release/ubuntu-25.04-server-cloudimg-amd64.img;534cbf0c44e86862535502f853829cefb771d19991892a31d14827d985829612"
+    "rocky;9110;tmpl-rocky-10;rocky-10.qcow2;https://dl.rockylinux.org/pub/rocky/10/images/x86_64/Rocky-10-GenericCloud-Base.latest.x86_64.qcow2;28628abf08a134c6f9e1eccbcac3f2898715919a6da294ae2c6cd66d6bc347ad"
+    "coreos;9123;tmpl-coreos-43;coreos.qcow2.xz;https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/43.20260119.3.1/x86_64/fedora-coreos-43.20260119.3.1-qemu.x86_64.qcow2.xz;76f1d1c22d09ac27a6ff2c78fc9418d82307f23a9fd558e40a289ff5a3212bcd"
+    "fedora;9143;tmpl-fedora-43;fedora-43.qcow2;https://download.fedoraproject.org/pub/fedora/linux/releases/43/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-43-1.6.x86_64.qcow2;846574c8a97cd2d8dc1f231062d73107cc85cbbbda56335e264a46e3a6c8ab2f"
+    "arch;9200;tmpl-arch-latest;arch-latest.qcow2;https://geo.mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-cloudinit.qcow2;https://geo.mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-cloudinit.qcow2.sha256"
+    "alpine;9319;tmpl-alpine-latest;alpine-latest.qcow2;https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/cloud/alpine-standard-3.19.1-x86_64-cloudinit-generic.qcow2;"
+)
+
 show_help() {
-    echo "${ylw}Usage:${rst} $0 [vmid] [name] [filename] [url]"
+    echo "${ylw}Usage:${rst} $0 [all|distro] [username]"
     echo ""
-    echo "  ${grn}No arguments:${rst} Runs default batch (Debian, Ubuntu, Fedora, Rocky, CoreOS)."
+    echo "  ${grn}No arguments or 'all':${rst} Runs default batch."
+    echo "  ${grn}Specific distro:${rst} $0 debian"
+    echo "  ${grn}Custom user:${rst} $0 all newuser"
+    echo "  ${grn}Available distros:${rst} all, $(get_distro_names)"
     echo "  ${grn}One-off input:${rst} $0 900 \"tmpl-custom\" \"image.qcow2\" \"https://example.com/image.qcow2\""
 }
 
@@ -135,7 +149,7 @@ create_template(){ # syntax: create_template <vmid> <name> <image> <url> [checks
 
     if ! manage_existing "$vmid"; then return; fi
     if ! webtest; then
-        echo "${red}CRITICAL: DNS Resolution or Internet failure.${rst}"
+        echo "${red}CRITICAL${rst}: DNS Resolution or Internet failure."
         return 1
     fi
 
@@ -158,7 +172,7 @@ create_template(){ # syntax: create_template <vmid> <name> <image> <url> [checks
     done
 
     if [[ "$success" != "true" ]]; then
-        echo "${red}Failed to acquire valid image after $max_attempts attempts. Moving to next task.${rst}"
+        echo "${ylw}Failed to acquire valid image after $max_attempts attempts.${rst} Moving to next task."
         return 1
     fi
 
@@ -169,8 +183,7 @@ create_template(){ # syntax: create_template <vmid> <name> <image> <url> [checks
     # Attempt to import disk
     echo "Importing disk to $storage..."
     if ! qm set "$vmid" --scsi0 "${storage}:0,import-from=$(pwd)/$image,discard=on"; then
-        echo "${ylw}Import failed. Checking if decompression is needed...${rst}"
-
+        echo "${ylw}Import failed.${rst} Checking if decompression is needed..."
         if [[ "$image" == *.xz ]]; then
             echo "Extracting .xz file..."
             xz -d "$image"
@@ -180,10 +193,9 @@ create_template(){ # syntax: create_template <vmid> <name> <image> <url> [checks
             gunzip "$image"
             image="${image%.gz}"
         fi
-
         echo "Retrying import with $image..."
         if ! qm set "$vmid" --scsi0 "${storage}:0,import-from=$(pwd)/$image,discard=on"; then
-            echo "${red}Failed to import disk even after extraction. Cleanup...${rst}"
+            echo "${red}Failed to import disk even after extraction.${rst} Cleanup..."
             rm -f "$image"
             qm destroy "$vmid"
             return 1
@@ -207,75 +219,79 @@ create_template(){ # syntax: create_template <vmid> <name> <image> <url> [checks
     echo "${grn}Successfully created template $vmid with username: ${username}${rst}"
 }
 
+download_distro() {
+    local distro_name="$1"
+    for distro_data in "${distros[@]}"; do
+        if [[ "$distro_data" == "$distro_name"* ]]; then
+            IFS=';' read -r -a params <<< "$distro_data"
+            create_template "${params[1]}" "${params[2]}" "${params[3]}" "${params[4]}" "${params[5]}"
+            return
+        fi
+    done
+    echo "${red}Distro '$distro_name' not found.${rst}"
+}
+
+get_distro_names() {
+    for distro_data in "${distros[@]}"; do
+        echo -n "$(echo "$distro_data" | cut -d';' -f1) "
+    done
+    echo ""
+}
+
+is_distro() {
+    local name="$1"
+    if [ "$name" == "all" ]; then
+        return 0
+    fi
+    for distro_data in "${distros[@]}"; do
+        if [[ "$distro_data" == "$name"* ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
 main(){
-    if [ "$#" -eq 4 ]; then
-        create_template "$1" "$2" "$3" "$4"
-    elif [ "$#" -gt 0 ]; then
-        show_help
+    if [ "$#" -ge 4 ] && ! is_distro "$1"; then
+        create_template "$1" "$2" "$3" "$4" "$5"
+        exit 0
+    fi
+
+    read -r -p "Current username will be: ${username}. Continue? (Y/n/[u]pdate): " choice
+    case "$choice" in
+        n|N )
+            echo "Exiting..."
+            exit 1
+            ;;
+        u|U )
+            while true; do
+                read -r -p "New username (alphanumeric, starts with letter): " new_username
+                # Regex: Starts with letter, followed by lowercase letters, numbers, or hyphens
+                if [[ "$new_username" =~ ^[a-z][a-z0-9-]*$ ]]; then
+                    username="$new_username"
+                    echo "Username updated to: $username"
+                    break
+                else
+                    echo "${red}Invalid username.${rst} Use lowercase letters, numbers, and hyphens only (start with a letter)."
+                fi
+            done
+            ;;
+        * ) ;;
+    esac
+
+    distro_to_download="${1:-all}"
+
+    if is_distro "$distro_to_download"; then
+        if [ "$distro_to_download" == "all" ]; then
+            for distro_data in "${distros[@]}"; do
+                distro_name=$(echo "$distro_data" | cut -d';' -f1)
+                download_distro "$distro_name"
+            done
+        else
+            download_distro "$distro_to_download"
+        fi
     else
-        # echo "Running default batch..."
-        read -r -p "Current username will be: ${username}. Continue? (Y/n/[u]pdate): " choice
-        case "$choice" in
-            n|N )
-                echo "Exiting..."
-                exit 1
-                ;;
-            u|U )
-                while true; do
-                    read -r -p "New username (alphanumeric, starts with letter): " new_username
-                    # Regex: Starts with letter, followed by lowercase letters, numbers, or hyphens
-                    if [[ "$new_username" =~ ^[a-z][a-z0-9-]*$ ]]; then
-                        username="$new_username"
-                        echo "Username updated to: $username"
-                        break
-                    else
-                        echo "${red}Invalid username.${rst} Use lowercase letters, numbers, and hyphens only (start with a letter)."
-                    fi
-                done
-                ;;
-            * ) ;;
-        esac
-        # create_template 912 "tmpl-debian-12" "debian-12.qcow2" \
-        # "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-genericcloud-amd64.qcow2"
-        create_template 9013 "tmpl-debian-13" "debian-13.qcow2" \
-        "https://cloud.debian.org/images/cloud/bookworm/20260210-2384/debian-12-genericcloud-amd64-20260210-2384.qcow2" \
-        "e5d776b9de352c89fbad4baec8bfd38a35c5905114a5f3b108946348cb44d869396d22e4a837a43afee4b11363d4759c358fb3e8a7cd07fa743ed6b663784fed"
--
-        # create_template 9024 "tmpl-ubuntu-24-04" "ubuntu-24-04.img" \
-        # "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img" \
-        # "2f9acc20a381cde6fa678672e20a1408d2b3f346531b9026df2a935c37d2de20"
-
-        create_template 9025 "tmpl-ubuntu-25" "ubuntu-25-04.img" \
-        "https://cloud-images.ubuntu.com/releases/plucky/release/ubuntu-25.04-server-cloudimg-amd64.img" \
-        "534cbf0c44e86862535502f853829cefb771d19991892a31d14827d985829612"
-
-        # "https://cloud-images.ubuntu.com/minimal/releases/plucky/release/ubuntu-25.04-minimal-cloudimg-amd64.img" \
-        # "9100043b7810ca4af933eeea7cb5c7d2806407b586086664b17254598550e71a"
-
-        # create_template 943 "tmpl-fedora-43" "fedora-43.qcow2" \
-        # "https://paducahix.mm.fcix.net/fedora/linux/releases/43/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-43-1.6.x86_64.qcow2"
-        create_template 9143 "tmpl-fedora-43" "fedora-43.qcow2" \
-        "https://download.fedoraproject.org/pub/fedora/linux/releases/43/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-43-1.6.x86_64.qcow2" \
-        "846574c8a97cd2d8dc1f231062d73107cc85cbbbda56335e264a46e3a6c8ab2f" # sha256
-
-        # create_template 9109 "tmpl-rocky-9" "rocky-9.qcow2" \
-        # "https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2" \
-        # "15d81d3434b298142b2fdd8fb54aef2662684db5c082cc191c3c79762ed6360c"
-
-        create_template 9110 "tmpl-rocky-10" "rocky-10.qcow2" \
-        "https://dl.rockylinux.org/pub/rocky/10/images/x86_64/Rocky-10-GenericCloud-Base.latest.x86_64.qcow2" \
-        "28628abf08a134c6f9e1eccbcac3f2898715919a6da294ae2c6cd66d6bc347ad" # sha256
-
-        # create_template 941 "tmpl-coreos" "coreos.qcow2.xz" \
-        # "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/41.20250125.3.0/x86_64/fedora-coreos-41.20250125.3.0-qemu.x86_64.qcow2.xz"
-        create_template 9123 "tmpl-coreos-43" "coreos.qcow2.xz" \
-        "https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/43.20260119.3.1/x86_64/fedora-coreos-43.20260119.3.1-qemu.x86_64.qcow2.xz" \
-        "76f1d1c22d09ac27a6ff2c78fc9418d82307f23a9fd558e40a289ff5a3212bcd" # sha256
-
-        # The NixOS OpenStack image link is incorrect
-        # create_template 9225 "tmpl-nixos-25-11" "nixos-25-11.qcow2" \
-        # "https://hydra.nixos.org/job/nixos/release-25.11/nixos.openstack-image.x86_64-linux/latest/download/1" \
-        # ""
+        show_help
     fi
 }
 
